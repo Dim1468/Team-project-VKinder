862e4cea48a93d75ece818a6e2144ae26ee1f113
Neto test <disr9197@gmail.com>
2024-05-08 12:17:35 +0500

    Improved logging

--- interaction with the VK bot.py
+++ interaction with the VK bot.py
@@ -1,21 +1,25 @@
+import logging
 import random
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from Token import community_token, user_token
 
-vk = vk_api.VkApi(token=community_token)
-longpoll = VkLongPoll(vk)
+# Логирование
+logging.basicConfig(filename='app.log', level=logging.INFO)
 
-vk_user_token = vk_api.VkApi(token=user_token)
+try:
+    vk = vk_api.VkApi(token=community_token)
+    longpoll = VkLongPoll(vk)
+    vk_user_token = vk_api.VkApi(token=user_token)
+except Exception as e:
+    logging.error(f"Error initializing VK API: {e}")
+    raise
 
 answer_list = ['Привет!', 'Приветствую!', 'Здравствуйте!']
-
-hello_list = ['Привет', 'привет', 'Хай', 'хай',
-              'Начать', 'Start', 'Начать поиск']
-
-# Список избранных контактов (пока не используется!)
+hello_list = ['Привет', 'привет', 'Хай', 'хай', 'Начать', 'Start', 'Начать поиск']
 favorite_users = []
+shown_users = []
 
 
 def write_msg(user_id, message, photos=None, photo_links=None, keyboard=None):
@@ -29,25 +33,23 @@ def write_msg(user_id, message, photos=None, photo_links=None, keyboard=None):
           photo_links (list, optional): Список ссылок на фотографии.
           keyboard (VkKeyboard, optional): Пользовательская клавиатура.
     """
-    params = {'user_id': user_id,
-              'random_id': random.randrange(10 ** 7)}
-
-    if message:
-        params['message'] = message
-
-    if keyboard is not None:
-        params['keyboard'] = keyboard
-
-    attachments = []
-    if photos:
-        attachments.extend(photos)
-    if photo_links:
-        attachments.extend(photo_links)
-
-    if attachments:
-        params['attachment'] = ','.join(attachments)
-
-    vk.method('messages.send', params)
+    try:
+        params = {'user_id': user_id, 'random_id': random.randrange(10 ** 7)}
+        if message:
+            params['message'] = message
+        if keyboard is not None:
+            params['keyboard'] = keyboard
+        attachments = []
+        if photos:
+            attachments.extend(photos)
+        if photo_links:
+            attachments.extend(photo_links)
+        if attachments:
+            params['attachment'] = ','.join(attachments)
+        vk.method('messages.send', params)
+    except Exception as e:
+        logging.error(f"Error sending message: {e}")
+        raise
 
 
 def get_user_profile(user_id):
@@ -57,8 +59,12 @@ def get_user_profile(user_id):
     Returns:
         dict: Информация о профиле пользователя.
     """
-    user_info = vk.method('users.get', {'user_ids': user_id, 'fields': 'sex,bdate,city'})
-    return user_info[0]
+    try:
+        user_info = vk.method('users.get', {'user_ids': user_id, 'fields': 'sex,bdate,city'})
+        return user_info[0]
+    except Exception as e:
+        logging.error(f"Error getting user profile: {e}")
+        raise
 
 
 def get_top_photos(user_id):
@@ -76,9 +82,10 @@ def get_top_photos(user_id):
         return photo_links
     except vk_api.exceptions.ApiError as e:
         if e.code == 30:
-            return []  # Возвращаем пустой список, если профиль закрыт
+            return []
         else:
-            raise e  # Пробрасываем ошибку дальше, если это другая ошибка
+            logging.error(f"Error getting top photos: {e}")
+            raise
 
 
 def get_city_id(city_name):
@@ -88,8 +95,15 @@ def get_city_id(city_name):
     Returns:
         int: ID города.
     """
-    city = vk_user_token.method('database.getCities', {'q': city_name})
-    return city['items'][0]['id'] if city['count'] > 0 else None
+    try:
+        city = vk_user_token.method('database.getCities', {'q': city_name})
+        return city['items'][0]['id'] if city['count'] > 0 else None
+    except KeyError as e:
+        logging.error(f"KeyError in get_city_id: {e}")
+        return None
+    except Exception as e:
+        logging.error(f"Error in get_city_id: {e}")
+        return None
 
 
 def create_next_button():
@@ -98,9 +112,13 @@ def create_next_button():
     Returns:
         VkKeyboard: Объект клавиатуры с кнопкой "Далее".
     """
-    next_button = VkKeyboard(one_time=False, inline=True)
-    next_button.add_button('Далее', color=VkKeyboardColor.PRIMARY)
-    return next_button
+    try:
+        next_button = VkKeyboard(one_time=False, inline=True)
+        next_button.add_button('Далее', color=VkKeyboardColor.PRIMARY)
+        return next_button
+    except Exception as e:
+        logging.error(f"Error in create_next_button: {e}")
+        return None
 
 
 def create_gender_keyboard():
@@ -109,14 +127,14 @@ def create_gender_keyboard():
     Returns:
         VkKeyboard: Объект клавиатуры с кнопками "Мужчина" и "Женщина".
     """
-    gender_keyboard = VkKeyboard(one_time=False, inline=True)
-    gender_keyboard.add_button('Мужчина', color=VkKeyboardColor.PRIMARY)
-    gender_keyboard.add_button('Женщина', color=VkKeyboardColor.PRIMARY)
-    return gender_keyboard
-
-
-# Список показанных пользователей для того, чтобы варианты не повторялись
-shown_users = []
+    try:
+        gender_keyboard = VkKeyboard(one_time=False, inline=True)
+        gender_keyboard.add_button('Мужчина', color=VkKeyboardColor.PRIMARY)
+        gender_keyboard.add_button('Женщина', color=VkKeyboardColor.PRIMARY)
+        return gender_keyboard
+    except Exception as e:
+        logging.error(f"Error in create_gender_keyboard: {e}")
+        return None
 
 
 def search_users(user_id, criteria, user_token):
@@ -126,68 +144,76 @@ def search_users(user_id, criteria, user_token):
           criteria (int): Критерий поиска (пол).
           user_token (str): Токен пользователя для доступа к API VK.
     """
-    user_profile = get_user_profile(user_id)
-    user_city_name = user_profile.get('city', {}).get('title', '')
-    user_city_id = get_city_id(user_city_name)
+    try:
+        user_profile = get_user_profile(user_id)
+        user_city_name = user_profile.get('city', {}).get('title', '')
+        user_city_id = get_city_id(user_city_name)
 
-    if user_city_id is None:
-        write_msg(user_id, 'Не удалось определить ваш город.')
-        return
+        if user_city_id is None:
+            write_msg(user_id, 'Не удалось определить ваш город.')
+            return
 
-    user_age = 2024 - int(user_profile['bdate'].split('.')[-1])
+        user_age = 2024 - int(user_profile['bdate'].split('.')[-1])
 
-    users = vk_api.VkApi(token=user_token).method('users.search',
-                                                  {'count': 10, 'fields': 'photo_200', 'sex': criteria,
-                                                   'city': user_city_id, 'age_from': user_age - 2,
-                                                   'age_to': user_age + 2})
-    if users['items']:
-        available_users = [user for user in users['items'] if user['id'] not in shown_users]
-        if available_users:
-            random_user = random.choice(available_users)
-            user_info = f"{random_user['first_name']} {random_user['last_name']}\nhttps://vk.com/id{random_user['id']}"
-            user_profile_link = f"https://vk.com/id{random_user['id']}"
-            write_msg(user_id, f"{user_info}\n")
+        users = vk_api.VkApi(token=user_token).method('users.search',
+                                                       {'count': 10, 'fields': 'photo_200', 'sex': criteria,
+                                                        'city': user_city_id, 'age_from': user_age - 2,
+                                                        'age_to': user_age + 2})
+        if users['items']:
+            available_users = [user for user in users['items'] if user['id'] not in shown_users]
+            if available_users:
+                random_user = random.choice(available_users)
+                user_info = f"{random_user['first_name']} {random_user['last_name']}\nhttps://vk.com/id{random_user['id'}"
+                user_profile_link = f"https://vk.com/id{random_user['id']}"
+                write_msg(user_id, f"{user_info}\n")
 
-            photo_links = get_top_photos(random_user['id'])
-            for link in photo_links:
-                write_msg(user_id, 'Фотография:', photo_links=[link])
+                photo_links = get_top_photos(random_user['id'])
+                for link in photo_links:
+                    write_msg(user_id, 'Фотография:', photo_links=[link])
 
-            shown_users.append(random_user['id'])
+                shown_users.append(random_user['id'])
 
-            next_button = create_next_button()
-            write_msg(user_id, "Хотите еще вариантов?", keyboard=next_button.get_keyboard())
+                next_button = create_next_button()
+                write_msg(user_id, "Хотите еще вариантов?", keyboard=next_button.get_keyboard())
+
+            else:
+                write_msg(user_id, 'Не удалось найти подходящих пользователей.')
 
         else:
             write_msg(user_id, 'Не удалось найти подходящих пользователей.')
 
-    else:
-        write_msg(user_id, 'Не удалось найти подходящих пользователей.')
+    except Exception as e:
+        logging.error(f"Error in search_users: {e}")
 
-
-criteria = None
+try:
+    criteria = None
+    
+except Exception as e:
+    logging.error(f"Error in other functions: {e}")
+    
 
 # Основной цикл
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            request = event.text
-
-            # Обработка приветствия
-            if request == 'Начать' or request in hello_list:
-                write_msg(event.user_id, f'{random.choice(answer_list)}')
-                write_msg(event.user_id,
-                          f'Подскажи, кого мы ищем?\n'
-                          f'Укажи пол:',
-                          keyboard=create_gender_keyboard().get_keyboard())
-
-            # Обработка выбора пола
-            elif request.lower() == 'мужчина' or request.lower() == 'женщина':
-                criteria = 2 if request.lower() == 'мужчина' else 1
-                search_users(event.user_id, criteria, user_token)
-
-            # Обработка запроса на дополнительные варианты
-            elif request == 'Далее':
-                if criteria:
+def main_loop():
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
+                request = event.text
+                if request == 'Начать' or request in hello_list:
+                    write_msg(event.user_id, f'{random.choice(answer_list)}')
+                    write_msg(event.user_id,
+                              f'Подскажи, кого мы ищем?\nУкажи пол:',
+                              keyboard=create_gender_keyboard().get_keyboard())
+                elif request.lower() == 'мужчина' or request.lower() == 'женщина':
+                    criteria = 2 if request.lower() == 'мужчина' else 1
                     search_users(event.user_id, criteria, user_token)
-                else:
-                    write_msg(event.user_id, 'Сначала укажите пол.')
+                elif request == 'Далее':
+                    if criteria:
+                        search_users(event.user_id, criteria, user_token)
+                    else:
+                        write_msg(event.user_id, 'Сначала укажите пол.')
+
+try:
+    main_loop()
+except Exception as e:
+    logging.error(f"Error in main loop: {e}")
+    raise
