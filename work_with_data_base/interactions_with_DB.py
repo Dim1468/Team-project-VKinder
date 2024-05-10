ef14a314a258cf5875cbd01c55f1513b7a260a03
Hleeroa <153171612+Hleeroa@users.noreply.github.com>
2024-05-10 10:33:37 +0500

    Update interactions_with_DB.py

--- work_with_data_base/interactions_with_DB.py
+++ work_with_data_base/interactions_with_DB.py
@@ -1,6 +1,51 @@
 import psycopg2
 
 
+class User:
+    def __init__(self, cur, gender=None, age=None, city=None, first_name=None, last_name=None,
+                 account_link=None, photo_links=None):
+        self.cur = cur
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.age = age
+        self.city = city
+        self.account_link = account_link
+        self.photo_links = photo_links
+
+    def get_a_person(self) -> tuple:
+        self.cur.execute('''
+            SELECT *
+            FROM user_account
+            WHERE
+                gender iLIKE %s
+                OR age = %s
+                OR city iLIKE %s
+            ORDER BY
+                gender iLIKE %s DESC, gender,
+                age = %s DESC, age,
+                city iLIKE %s DESC, city;   
+        ''', (self.gender, self.age, self.city, self.gender, self.age, self.city))
+        return self.cur.fetchone()
+
+    def check_double(self) -> bool:
+        self.cur.execute(f'''
+            SELECT count(1) > 0
+            FROM user_account
+            WHERE account_link = '{self.account_link}'
+            ''')
+        result = self.cur.fetchone()
+        return result[0]
+
+    def put_a_person(self):
+        if self.check_double() is not True:
+            self.cur.execute('''
+                INSERT INTO user_account (first_name, last_name, gender, age, city, account_link, photo_links)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ''', (self.first_name, self.last_name, self.gender, self.age,
+                  self.city, self.account_link, self.photo_links))
+
+
 def id_by_link(cur, account_link) -> int:
     cur.execute('''
         SELECT id 
@@ -8,42 +53,8 @@ def id_by_link(cur, account_link) -> int:
         WHERE account_link = %s
     ''', (account_link,))
     user_id = cur.fetchone()[0]
+    print(user_id)
     return user_id
-
-
-def get_a_person(cur, gender=None, age=None, city=None) -> tuple:
-    cur.execute('''
-        SELECT *
-        FROM user_account
-        WHERE
-            gender iLIKE %s
-            OR age = %s
-            OR city iLIKE %s
-        ORDER BY
-            gender iLIKE %s DESC, gender,
-            age = %s DESC, age,
-            city iLIKE %s DESC, city;   
-    ''', (gender, age, city, gender, age, city))
-    print(cur.fetchone())
-    return cur.fetchone()
-
-
-def check_double(cur, account_link) -> bool:
-    cur.execute(f'''
-        SELECT count(1) > 0
-        FROM user_account
-        WHERE account_link = '{account_link}'
-        ''')
-    result = cur.fetchone()
-    return result[0]
-
-
-def put_a_person(cur, first_name, last_name, account_link, gender=None, age=None, city=None, photo_links=None):
-    if check_double(cur, account_link) is not True:
-        cur.execute('''
-            INSERT INTO user_account (first_name, last_name, gender, age, city, account_link, photo_links)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-        ''', (first_name, last_name, gender, age, city, account_link, photo_links))
 
 
 def to_like(conn, user_id, like_id):
