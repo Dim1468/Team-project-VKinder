bf2b44d6be2a53f9287b350a6449b202f722bbf9
Hleeroa <153171612+Hleeroa@users.noreply.github.com>
2024-05-10 12:44:19 +0500

    Classified main.py

--- work_with_data_base/db_test/main.py
+++ work_with_data_base/db_test/main.py
@@ -1,6 +1,6 @@
 import psycopg2
 from psycopg2.extras import Json
-from work_with_data_base.interactions_with_DB import (User, Actions)
+from work_with_data_base.interactions_with_DB import (User)
 from work_with_data_base.creation_of_DB import renovate_tables
 from work_with_data_base.user_data.DB_login_info import database, user, password
 
@@ -9,26 +9,19 @@ if __name__ == '__main__':
         with conn.cursor() as cur:
 
             # Создаю пользователей
-            put_user1 = User(cur, 'female', None, 'Stokholm', 'Maria',
+            put_user1 = User(conn, cur, 'female', None, 'Stokholm', 'Maria',
                              'Reinolds', 'jbjkcmck',
                              photo_links=Json({
                                  1: 'https://i.yapx.cc/OMDU5.jpg',
                                  2: 'https://i.pinimg.com/736x/7c/24/cc/7c24ccdd8698cce9aa18b13ec6b59082.jpg',
                                  3: 'https://www.youtube.com/watch?v=1HVWTrbgmxw'
                              }))
-            put_user2 = User(cur, 'male', '18', 'Stokholm', 'Leo', 'Peterson', 'sxjdvbkbc')
-            put_user3 = User(cur, None, '13', 'Valle del sol', 'Xio', 'Mala-Suerte', 'israpsidian')
+            put_user2 = User(conn, cur, 'male', '18', 'Stokholm', 'Leo', 'Peterson', 'sxjdvbkbc')
+            put_user3 = User(conn, cur, None, '13', 'Valle del sol', 'Xio', 'Mala-Suerte', 'israpsidian')
 
-            get_user1 = User(cur, city='Stokholm')
-            get_user2 = User(cur, gender='male', age='18', city='Stokholm')
-            get_user3 = User(cur, age='13', city='valle del sol')
-
-            like1 = Actions(conn, 1, like_id=3)
-            like2 = Actions(conn, 7, like_id=3)
-            like3 = Actions(conn, 3, like_id=3)
-            block1 = Actions(conn, 2, block_id=1)
-            block2 = Actions(conn, 3, block_id=3)
-            block3 = Actions(conn, 15, block_id=1)
+            get_user1 = User(conn, cur, city='Stokholm')
+            get_user2 = User(conn, cur, gender='male', age='18', city='Stokholm')
+            get_user3 = User(conn, cur, age='13', city='valle del sol')
 
             # Очищаю данные таблицы
             renovate_tables(cur)
@@ -44,11 +37,11 @@ if __name__ == '__main__':
             get_user3.get_a_person()
 
     # тестирую функцию to_like
-    like1.to_like()
-    like2.to_like()
-    like3.to_like()
+    put_user2.to_like(3)
+    put_user2.to_like(2)
+    put_user2.to_like(15)
 
     # тестирую функцию to_block
-    block1.to_block()
-    block2.to_block()
-    block3.to_block()
+    put_user1.to_block(1)
+    put_user1.to_block(19)
+    put_user1.to_block(2)
